f9ac01a4def515efcca888ffe6445f9df4b38073
eli <eli@anthropic.com>
2022-05-27 11:53:54 -0700

    fix syntax errs
    Former-commit-id: ce0bbfdef70fcd86284223998877b32017b5a89c

--- jitlog/test/data/code.py
+++ jitlog/test/data/code.py
@@ -1,7 +1,8 @@
-def add(a + b): # this should be line 1
+def add(a, b):  # this should be line 1
     return a + b
 
-def mul(a): # 4
+
+def mul(a):  # 4
     c = a * 2
-	d = c * 3
+    d = c * 3
     return d + 5
